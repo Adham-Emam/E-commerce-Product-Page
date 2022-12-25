41686c059086118732165357f22f6f2492baa277
Adham_Emam <mrado372002@gmail.com>
2022-12-25 07:28:40 +0200

    upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,86 @@
+// Cart Function
+let cartIcon = document.querySelector(".cart > img");
+let cartList = document.querySelector(".your-cart");
+let cartLength = document.querySelector(".cart span");
+
+let closeCart = document.createElement("div");
+closeCart.className = "close-cart";
+document.body.appendChild(closeCart);
+
+cartIcon.addEventListener("click", () => {
+  cartList.classList.toggle("active");
+  closeCart.classList.toggle("active");
+});
+
+closeCart.addEventListener("click", () => {
+  closeCart.classList.remove("active");
+  cartList.classList.remove("active");
+});
+// Add to Cart
+let addToCart = document.querySelector(".add-cart");
+
+let productsDiv = document.createElement("div");
+productsDiv.className = "products-div";
+cartList.appendChild(productsDiv);
+
+let checkout = document.createElement("button");
+checkout.textContent = "Checkout";
+checkout.className = "checkout";
+
+addToCart.addEventListener("click", () => {
+  cartList.classList.add("active");
+  closeCart.classList.add("active");
+
+  let product = document.createElement("div");
+  product.className = "product";
+
+  let productImg = document.createElement("img");
+  productImg.className = `product-img`;
+  productImg.src = selectedImg.src;
+
+  let productTitle = document.createElement("h5");
+  productTitle.textContent = document.querySelector("h1").textContent;
+
+  let productPrice = document.createElement("p");
+  productPrice.innerHTML = `$125.00 x ${quantity.innerHTML}  <span>$${price.textContent}.00</span>`;
+
+  let removeProduct = document.createElement("img");
+  removeProduct.className = "remove";
+  removeProduct.src = "images/icon-delete.svg";
+
+  cartLength.classList.add("active");
+  cartLength.textContent++;
+
+  removeProduct.addEventListener("click", () => {
+    product.remove();
+
+    cartLength.textContent--;
+
+    if (cartLength.textContent == 0) {
+      cartLength.classList.remove("active");
+    }
+
+    if (!productsDiv.innerHTML) {
+      checkout.remove();
+      let emptyMessage = document.createElement("p");
+      emptyMessage.textContent = "Your cart is empty";
+      cartList.appendChild(emptyMessage);
+    }
+  });
+
+  product.appendChild(productImg);
+  product.appendChild(productTitle);
+  product.appendChild(productPrice);
+  product.appendChild(removeProduct);
+  productsDiv.appendChild(product);
+
+  if (document.querySelector(".your-cart > p")) {
+    document.querySelector(".your-cart > p").remove();
+
+    cartList.appendChild(checkout);
+  }
+});
+
 // Ressponsive List
 let ressponsiveIcon = document.querySelector(".ressponsive-icon");
 let ressponsiveList = document.querySelector(".ressponsive-list");
@@ -25,6 +108,8 @@ let gallery = document.querySelector(".gallery");
 let selectedImg = document.querySelector(".selected-img  img");
 
 selectedImg.addEventListener("click", (e) => {
+  cartList.classList.remove("active");
+
   let popUp = document.createElement("div");
   popUp.className = "popup";
 
